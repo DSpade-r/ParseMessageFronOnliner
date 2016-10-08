95ff63e4645486a9aa3c276d5379d4ed81e92719
MSI-PC <stalk.sdv@gmail.com>
2016-10-08 13:36:06 +0300

    Сделана рабочая логика парсинга и все методы покрыты тестами

--- UserMessages.Tests/ParseHTML.Test.cs
+++ UserMessages.Tests/ParseHTML.Test.cs
@@ -78,5 +78,43 @@ namespace UserMessages.Tests
             //Assert
             Assert.AreEqual(userId, testUserId);
         }
+        [TestMethod]
+        public void Test_GetPostNode()
+        {
+            //GetPostNode по логике свое работы должен возвращать 20 нодов(постов) + 1 пост это шапка(фиксированный пост)
+            //с каждой заполненной на форуме страницы - это и проверю
+            //каждая запись в listNode должна иметь имя ноды - li
+            //Arrange
+            HtmAgilityParser parser = new HtmAgilityParser();
+            ParseInfo parseInfoTest = new ParseInfo { MaxMessage = 4, Name = "1", PageCount = 1, UserID = 1, url = @"http://forum.onliner.by/viewtopic.php?t=1863541&start=8340" };
+            //Act
+            List<HtmlNode> listNode = parser.GetPostNode(parseInfoTest);
+            int countNode = 21;
+            string nameItemOfNode = "li";
+            //Assert
+            Assert.AreEqual(countNode, listNode.Count);
+            Assert.AreEqual(nameItemOfNode, listNode[2].Name);
+        }
+        [TestMethod]
+        public void Test_ParseHtmlOnliner()
+        {
+            //Arrange
+            HtmAgilityParser parser = new HtmAgilityParser();
+            ParseInfo parseInfoTest = new ParseInfo { MaxMessage = 20, Name = "DSpade", PageCount = 10, UserID = 1, url = @"http://forum.onliner.by/viewtopic.php?t=1863541&start=16360" };
+            NodeOfParse testNote = new NodeOfParse();
+            testNote.Date = new DateTime(2016, 09, 22, 21, 24, 00);
+            testNote.IdMessage = 91125917;
+            testNote.IdUser = 371661;
+            testNote.NameUser = "DSpade";
+            testNote.Message = "<p><strong>ирэн-12</strong>, ерунда все это. Не будьте такими пугаными<img src=\"http://forum.onliner.by/images/smilies/icon_smile.gif\"></p>";
+            //Act
+            List<NodeOfParse> Notes = parser.ParseHtmlOnliner(parseInfoTest);
+            //Assert
+            Assert.AreEqual(Notes[0].Date, testNote.Date);
+            Assert.AreEqual(Notes[0].IdMessage, testNote.IdMessage);
+            Assert.AreEqual(Notes[0].IdUser, testNote.IdUser);
+            Assert.AreEqual(Notes[0].NameUser, testNote.NameUser);
+            Assert.AreEqual(Notes[0].Message, testNote.Message);
+        }
     }
 }
--- UserMessages/Infrastructure/HtmAgilityParser.cs
+++ UserMessages/Infrastructure/HtmAgilityParser.cs
@@ -28,37 +28,52 @@ namespace UserMessages.Infrastructure
         public List<NodeOfParse> ParseHtmlOnliner(ParseInfo parseInfo)
         {
             List<NodeOfParse> Notes = new List<NodeOfParse>();
-            //получаю коллекцию постов на странице из parseInfo
-            var liList = GetPostNode(parseInfo);
-            //просматриваю все посты и записываю те, которые удовлетворяют условию
-            foreach (var node in liList)
+            //цикл для перебора страниц парсинга
+            for (int parsePage = 1; parsePage <= parseInfo.PageCount; parsePage++)
             {
-                //получаю никнейм пользователя
-                var author = node.Descendants().
-                    Where(div => div.GetAttributeValue("class", "").Equals("b-mtauthor")).Single().
-                    Descendants().
-                    Where(div => div.GetAttributeValue("class", "").Equals("b-mtauthor-i")).Single().
-                    SelectSingleNode("big").
-                    SelectSingleNode("span").
-                    SelectSingleNode("a").InnerText;
-                //попровки по кодировке onlinet.by utf-8 использует
-                byte[] bytes = Encoding.Default.GetBytes(author);
-                author = Encoding.UTF8.GetString(bytes);
-                int countNote = 0; //количество записей по автору
-                //проверяю "наш ли клиент":)
-                if (author == parseInfo.Name)
+                //формирую url в зависимости от количества необходимых страниц
+                string pattern = @"=\d{1,10}$"; //паттерн для определения количества постов
+                Regex url = new Regex(pattern);
+                string strNumberPage = url.Match(parseInfo.url).Value;  //нахожу страницу форума из url(нужня для модификации)
+                strNumberPage = strNumberPage.Remove(0, 1);             //удаляю "=" для корректного преобразования в int
+                //int forumList = Int32.Parse(strNumberPage)
+                strNumberPage = (Int32.Parse(strNumberPage) + (parsePage == 1? 0 : 20)).ToString(); //преобразовываю в int, модифицирую значение на след страницу и возвращаю обратно в string
+                strNumberPage = "=" + strNumberPage;
+                parseInfo.url = url.Replace(parseInfo.url, strNumberPage); //получаю конечную строку с новым адресом страницы
+                //получаю коллекцию постов на странице из parseInfo
+                var liList = GetPostNode(parseInfo);
+                //просматриваю все посты и записываю те, которые удовлетворяют условию
+                foreach (var node in liList)
                 {
-                    //вбиваю в коллекцию данные по записи:
-                    //1. id сообщения
-                    //2. дата и время сообщения
-                    //3. само солбщение
-                    if (Notes.Count < parseInfo.MaxMessage)
+                    //получаю никнейм пользователя
+                    var author = node.Descendants().
+                        Where(div => div.GetAttributeValue("class", "").Equals("b-mtauthor")).Single().
+                        Descendants().
+                        Where(div => div.GetAttributeValue("class", "").Equals("b-mtauthor-i")).Single().
+                        SelectSingleNode("big").
+                        SelectSingleNode("span").
+                        SelectSingleNode("a").InnerText;
+                    //попровки по кодировке onlinet.by utf-8 использует
+                    byte[] bytes = Encoding.Default.GetBytes(author);
+                    author = Encoding.UTF8.GetString(bytes);
+                    //проверяю "наш ли клиент":)
+                    if (author == parseInfo.Name)
                     {
-                        Notes[countNote].Message = GetMessage(node);
-                        Notes[countNote].IdMessage = GetIdMessage(node);
-                        Notes[countNote].NameUser = author;
-                        Notes[countNote].Date = GetDateMessage(node);
-                        Notes[countNote].IdUser = GetUserID(node);
+                        //Усли уже найдено необходимо количество сообщений - то заканчиваем дальше работать 
+                        //и возвращаем то что есть
+                        if (Notes.Count == parseInfo.MaxMessage) return Notes;
+                        //вбиваю в коллекцию данные по записи:
+                        //1. id сообщения
+                        //2. дата и время сообщения
+                        //3. само солбщение    
+                        NodeOfParse temp = new NodeOfParse();
+                        temp.IdMessage = GetIdMessage(node);
+                        if (Notes.Find(it => it.IdMessage == temp.IdMessage) != null) continue;
+                        temp.Message = GetMessage(node);                        
+                        temp.NameUser = author;
+                        temp.Date = GetDateMessage(node);
+                        temp.IdUser = GetUserID(node);
+                        Notes.Add(temp);                        
                     }
                 }
             }
@@ -151,8 +166,9 @@ namespace UserMessages.Infrastructure
             string innerDate = TagWithIdMessage[0].SelectSingleNode("span").InnerText;
             //получаю русские символы
             byte[] bytes = Encoding.Default.GetBytes(innerDate);
-            innerDate = Encoding.UTF8.GetString(bytes);          
-            DateTime innerDateDT = DateTime.ParseExact(innerDate, "dd MMMM yyyy HH:mm", CultureInfo.CreateSpecificCulture("ru-RU"));
+            innerDate = Encoding.UTF8.GetString(bytes);
+            string[] formats = { "dd MMMM yyyy HH:mm", "d MMMM yyyy HH:mm" };
+            DateTime innerDateDT = DateTime.ParseExact(innerDate, formats, CultureInfo.CreateSpecificCulture("ru-RU"), DateTimeStyles.None);
             return innerDateDT; 
         }
         #endregion
