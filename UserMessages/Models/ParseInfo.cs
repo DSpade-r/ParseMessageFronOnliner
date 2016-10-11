3adb79adc49e96c843ed5e4b7908161efcc61f52
MSI-PC <stalk.sdv@gmail.com>
2016-10-11 11:17:58 +0300

    Добавлена валидация

--- UserMessages/Models/ParseInfo.cs
+++ UserMessages/Models/ParseInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,16 @@ namespace UserMessages.Models
 {
     public class ParseInfo
     {
+        [Required(ErrorMessage = "Введите никнейм пользователя")]
         public string Name { set; get; }
         public int UserID { set; get; }
+        [Required(ErrorMessage = "Введите макимальное количество сообщений для поимка")]
+        [Range(1,5000,ErrorMessage ="Количество сообщений 1-5000")]
         public int MaxMessage { set; get; }
+        [Required(ErrorMessage = "Введите количество просматриваемых страниц")]
+        [Range(1,1000,ErrorMessage = "Количество страниц 1-1000")]
         public int PageCount { set; get; }
+        [Required(ErrorMessage = "Введите адрес начальной страницы просмотра сообщений")]
         public string url { set; get; }
     }
 }
